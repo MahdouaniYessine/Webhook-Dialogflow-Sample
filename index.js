0c2b973de0644e1f33217c2be03395bd945a0efb
Yessine Mahdouani <yessine.mahdouani@interaktive.it>
2018-07-24 12:04:41 +0200

    0.1

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var exps = express();
+
+const ApiAiApp = require('actions-on-google');
+
+exps.use(bodyParser.json());
+
+// API.AI actions
+const WELCOME_ACTION = 'input.welcome';
+
+exps.post('/test', function(request, response) {
+  const app = new ApiAiApp({request, response});
+  function greetUser (app) {
+    app.tell("Hello World!");
+  }
+
+  let actionMap = new Map();
+  actionMap.set(WELCOME_ACTION, greetUser);
+
+  app.handleRequest(actionMap);
+});
+
+exps.listen((process.env.PORT || 7001), function() {
+    console.log("App up and running, listening.")
+})
