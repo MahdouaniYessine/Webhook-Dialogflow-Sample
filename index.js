663a5802f34b1be0c6b45659e30cde58de5c709d
Yessine Mahdouani <yessine.mahdouani@interaktive.it>
2018-07-24 17:46:52 +0200

    0.31

--- index.js
+++ index.js
@@ -15,47 +15,56 @@ server.use(bodyParser.json());
 
 server.post('/test', (req, res) => {
 
+  var notFoundResponseEN="I wasn't able to find the requested radio. Please, try to articulate the radio's name or ask for another station. So, which radio station would you like to listen to?";
+
   var request = require('request');
   request.post({url:'http://zappingradioapp.com/mobileapp/zappingapi.php', form: {key:"rtl",action:6}},
                 function (error, response, body) {
 
                   if (!error && response.statusCode == 200) {
-                      var info = JSON.parse(body);
-                     return res.json(info)
+                      var result = JSON.parse(body);
+                      if(result["success"]==1){
+
+                        return res.json({
+                        source: 'test-webhook-zapping',
+                        "payload": {
+                          "google": {
+                            "expectUserResponse": true,
+                            "richResponse": {
+                              "items": [{  "simpleResponse": {  "textToSpeech": "Play Radio "+result["name"]}}
+                              ,{"mediaResponse": {
+                                  "mediaType": "AUDIO",
+                                      "mediaObjects": [
+                                        {
+                                          "name": result["name"],
+                                          "largeImage": {
+                                            "url": "http://www.zappingradioapp.com"+result["image"],
+                                            "accessibilityText": "Media icon"
+                                          },
+                                          "contentUrl": result["stream"]
+                                        }
+                                      ]
+                                    }
+                                  }
+                              ],
+                                "suggestions": [
+                                  {
+                                    "title": "Pause Radio"
+                                  }
+                                ]
+                            }
+                          }
+                        }
+                      });
+
+                      }else{
+                        return res.json({fulfillmentText:notFoundResponseEN})
+                      }
+                    }
+                    else{
+                      return res.json({fulfillmentText:notFoundResponseEN})
                     }
   });
-
-  /*return res.json({
-  source: 'test-webhook-zapping',
-  "payload": {
-    "google": {
-      "expectUserResponse": true,
-      "richResponse": {
-        "items": [{  "simpleResponse": {  "textToSpeech": "Play Radio RTL"}}
-        ,{"mediaResponse": {
-            "mediaType": "AUDIO",
-                "mediaObjects": [
-                  {
-                    "name": "RDS",
-                    "largeImage": {
-                      "url": "http://www.zappingradioapp.com/images/italia/6/11106.png",
-                      "accessibilityText": "Media icon"
-                    },
-                    "contentUrl": "http://46.37.20.205:8000/rdsmp3"
-                  }
-                ]
-              }
-            }
-        ],
-          "suggestions": [
-            {
-              "title": "Pause Radio"
-            }
-          ]
-      }
-    }
-  }
-});*/
 
 });
 
