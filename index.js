69028f40d2579e0761d50d29c8066af1d974b554
Yessine Mahdouani <yessine.mahdouani@interaktive.it>
2018-07-24 17:52:00 +0200

    3.1

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ server.post('/test', (req, res) => {
   var notFoundResponseEN="I wasn't able to find the requested radio. Please, try to articulate the radio's name or ask for another station. So, which radio station would you like to listen to?";
 
   var request = require('request');
-  request.post({url:'http://zappingradioapp.com/mobileapp/zappingapi.php', form: {key:"rtl",action:6}},
+
+  request.post({url:'http://zappingradioapp.com/mobileapp/zappingapi.php', form: {key:req.body.result.parameters.any,action:6}},
                 function (error, response, body) {
 
                   if (!error && response.statusCode == 200) {
