8c927fa871f04bde15601f56ec813a7cba14a6fd
Yessine Mahdouani <yessine.mahdouani@interaktive.it>
2018-07-24 14:07:39 +0200

    0.17

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { dialogflow } = require('actions-on-google');
 
-const app = dialogflow();
+const server = express();
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.intent('test', conv => {
-  conv.ask('really?');
+server.post('/test', (req, res) => {
+
+        return res.json({
+            speech: 'Something went wrong!',
+            displayText: 'Something went wrong!',
+            source: 'Zapping Radio +'
+        });
+
 });
 
-
-express().use(bodyParser.json(), app).listen(3000);
+server.listen((process.env.PORT || 8000), () => {
+    console.log("Server is up and running...");
+});
