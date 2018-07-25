161aa58cf778d82b920b3f24c1f679f0b92bc2d3
Yessine Mahdouani <yessine.mahdouani@interaktive.it>
2018-07-25 14:08:09 +0200

    0.48

--- index.js
+++ index.js
@@ -14,8 +14,13 @@ server.use(bodyParser.urlencoded({
 server.use(bodyParser.json());
 
 server.post('/test', (req, res) => {
+  var notFoundResponseEN="I wasn't able to find the requested radio. Please, try to articulate the radio's name or ask for another station. So, which radio station would you like to listen to?";
 
-return res.json({fulfillmentText:req.body.queryResult.parameters.any+" "+req.body.queryResult.languageCode.substr(0,2)})
+switch(req.body.queryResult.languageCode.substr(0,2)){
+  case "it":  notFoundResponseEN="Non sono riuscita a trovare la radio richiesta. Prova a scandire il nome o cambia stazione, per favore. Allora, quale radio vorresti ascoltare?";
+    break;
+}
+return res.json({fulfillmentText:req.body.queryResult.parameters.any+", "+notFoundResponseEN})
 
 });
 
